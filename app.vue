42a9d3a5979e90f3eaa35dd8c727787698c6c020
tangchaoren131 <735405280@qq.com>
2018-01-14 16:01:11 +0800

    add list

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
    <div>
        <div class='header'>
-
+           
        </div>
    </div>
 </template>
---
+++ router.js
@@ -0,0 +1,7 @@
+export default [{
+    path: './list',
+    meta: {
+        title: '商品列表'
+    },
+    component: (resolve) => require(['./views/list.vue'])
+}]
